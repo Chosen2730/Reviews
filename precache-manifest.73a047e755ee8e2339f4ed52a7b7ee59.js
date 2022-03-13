1f45d6b9469b83363bd5c48a367831e6c54014ba
Chosen2730 <70141158+Chosen2730@users.noreply.github.com>
2022-03-13 16:53:52 +0100

    Updates

---
+++ precache-manifest.73a047e755ee8e2339f4ed52a7b7ee59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "181c0b4c1e43aaaa5274d331704eb82e",
+    "url": "/Reviews/index.html"
+  },
+  {
+    "revision": "edd8dcfddbb497b3afe7",
+    "url": "/Reviews/static/css/main.e64ab053.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/Reviews/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Reviews/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edd8dcfddbb497b3afe7",
+    "url": "/Reviews/static/js/main.2609dddc.chunk.js"
+  },
+  {
+    "revision": "5cfec122a862e1e7b69a",
+    "url": "/Reviews/static/js/runtime-main.949f2259.js"
+  }
+]);
